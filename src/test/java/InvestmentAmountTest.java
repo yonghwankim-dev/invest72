f025b1d1f7a1dde8fddfd33c01ebb800a17463ab
yonghwankim-dev <dragonbead95@naver.com>
2025-06-12 16:07:39 +0900

    add interface - InvestmentAmount

---
+++ src/test/java/InvestmentAmountTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class InvestmentAmountTest {
+	@Test
+	void created(){
+		InvestmentAmount amount = new MonthlyInvestmentAmount();
+		Assertions.assertNotNull(amount);
+	}
+}
