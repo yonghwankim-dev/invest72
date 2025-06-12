3a105b79255461b3175970748d002052647f31aa
yonghwankim-dev <dragonbead95@naver.com>
2025-06-12 13:31:49 +0900

    add test - getPrincipal

--- src/test/java/InvestmentSummaryTest.java
+++ src/test/java/InvestmentSummaryTest.java
@@ -7,4 +7,14 @@ class InvestmentSummaryTest {
 		InvestmentSummary summary = new CompoundMonthlyInvestmentSummary();
 		Assertions.assertNotNull(summary);
 	}
+
+	@Test
+	void getPrincipal_shouldReturnPrincipal(){
+		InvestmentSummary summary = new CompoundMonthlyInvestmentSummary();
+
+		int principal = summary.getPrincipal();
+
+		int expected = 0;
+		Assertions.assertEquals(expected, principal);
+	}
 }
