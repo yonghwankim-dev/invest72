83ad7995ef542792312b6dc8cc4ea0a538dc5ce4
yonghwankim-dev <dragonbead95@naver.com>
2025-06-12 16:21:48 +0900

    introduce field - monthlyAmount

--- src/test/java/InvestmentAmountTest.java
+++ src/test/java/InvestmentAmountTest.java
@@ -1,17 +1,24 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InvestmentAmountTest {
+
+	private int monthlyAmount;
+
+	@BeforeEach
+	void setUp() {
+		monthlyAmount = 1_000_000;
+	}
+
 	@Test
 	void created(){
-		int monthlyAmount = 1_000_000;
 		InvestmentAmount investmentAmount = new MonthlyInvestmentAmount(monthlyAmount);
 		Assertions.assertNotNull(investmentAmount);
 	}
 
 	@Test
 	void shouldReturnAmount(){
-		int monthlyAmount = 1_000_000;
 		InvestmentAmount investmentAmount = new MonthlyInvestmentAmount(monthlyAmount);
 
 		int amount = investmentAmount.getAmount();
