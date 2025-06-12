bf3e4eba0cb3ff2b2de8e84635932b9619f0b7b1
yonghwankim-dev <dragonbead95@naver.com>
2025-06-12 14:41:22 +0900

    add test case - annualInterestRate is zero

--- src/test/java/InvestmentCalculatorTest.java
+++ src/test/java/InvestmentCalculatorTest.java
@@ -88,4 +88,17 @@ class InvestmentCalculatorTest {
 		Assertions.assertThrows(IllegalArgumentException.class,
 			() -> calculator.calculate(monthlyInvestment, investmentPeriod, annualInterestRate));
 	}
+
+	@Test
+	void shouldReturnSummary_whenAnnualInterestRateIsZero(){
+		annualInterestRate = 0.0;
+
+		InvestmentSummary summary = calculator.calculate(monthlyInvestment, investmentPeriod, annualInterestRate);
+
+		int expectedPrincipal = 12_000_000;
+		int expectedInterest = 0;
+		InvestmentSummary expected = new CompoundMonthlyInvestmentSummary(expectedPrincipal, expectedInterest);
+		Assertions.assertEquals(expected.getPrincipal(), summary.getPrincipal());
+		Assertions.assertEquals(expected.getInterest(), summary.getInterest());
+	}
 }
