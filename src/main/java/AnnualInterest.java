c8c870086a9982485e46eeecab82999e71b216e7
yonghwankim-dev <dragonbead95@naver.com>
2025-06-12 15:45:44 +0900

    add field in AnnualInterest

--- src/main/java/AnnualInterest.java
+++ src/main/java/AnnualInterest.java
@@ -1,7 +1,13 @@
 public class AnnualInterest implements Interest {
 
+	private final double annualRate;
+
+	public AnnualInterest(double annualRate) {
+		this.annualRate = annualRate;
+	}
+
 	@Override
 	public double getAnnualRate() {
-		return 0;
+		return this.annualRate;
 	}
 }
--- src/test/java/AnnualInterestTest.java
+++ src/test/java/AnnualInterestTest.java
@@ -2,9 +2,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class AnnualInterestTest {
+
 	@Test
 	void created(){
-		Interest interest = new AnnualInterest();
+		double annualRate = 0.0;
+		Interest interest = new AnnualInterest(annualRate);
 		Assertions.assertNotNull(interest);
+	}
+
+	@Test
+	void shouldReturnMonthlyRate_givenAnnualRateValue(){
+		double annualRate = 0.05;
+		Interest interest = new AnnualInterest(annualRate);
+
+		double actualAnnualRate = interest.getAnnualRate();
+
+		double expectedAnnualRate = 0.05;
+		Assertions.assertEquals(expectedAnnualRate, actualAnnualRate, 0.0001);
 	}
 }
