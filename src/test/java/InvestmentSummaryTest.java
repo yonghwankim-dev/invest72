e6248ab07e93366858340dcfe05af8d9393ce26b
yonghwankim-dev <dragonbead95@naver.com>
2025-06-12 13:34:48 +0900

    introduce field - principal

--- src/test/java/InvestmentSummaryTest.java
+++ src/test/java/InvestmentSummaryTest.java
@@ -1,17 +1,24 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InvestmentSummaryTest {
+
+	private int principal;
+
+	@BeforeEach
+	void setUp() {
+		principal = 12_000_000;
+	}
+
 	@Test
 	void created(){
-		int principal = 12_000_000;
 		InvestmentSummary summary = new CompoundMonthlyInvestmentSummary(principal);
 		Assertions.assertNotNull(summary);
 	}
 
 	@Test
 	void getPrincipal_shouldReturnPrincipal(){
-		int principal = 12_000_000;
 		InvestmentSummary summary = new CompoundMonthlyInvestmentSummary(principal);
 
 		int actual = summary.getPrincipal();
