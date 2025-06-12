c2272b989e5e9b5e486e2f39c8d875ffeae02b90
yonghwankim-dev <dragonbead95@naver.com>
2025-06-12 15:46:26 +0900

    fix test name

--- src/test/java/AnnualInterestTest.java
+++ src/test/java/AnnualInterestTest.java
@@ -1,23 +1,31 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AnnualInterestTest {
 
+	private double annualRate;
+	private double delta;
+
+	@BeforeEach
+	void setUp() {
+		annualRate = 0.0;
+		delta = 0.0001;
+	}
+
 	@Test
 	void created(){
-		double annualRate = 0.0;
 		Interest interest = new AnnualInterest(annualRate);
 		Assertions.assertNotNull(interest);
 	}
 
 	@Test
-	void shouldReturnMonthlyRate_givenAnnualRateValue(){
-		double annualRate = 0.05;
+	void shouldReturnAnnualRate_givenAnnualRateValue(){
 		Interest interest = new AnnualInterest(annualRate);
 
 		double actualAnnualRate = interest.getAnnualRate();
 
 		double expectedAnnualRate = 0.05;
-		Assertions.assertEquals(expectedAnnualRate, actualAnnualRate, 0.0001);
+		Assertions.assertEquals(expectedAnnualRate, actualAnnualRate, delta);
 	}
 }
