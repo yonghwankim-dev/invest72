30dd8756730fad45edb52992c5bf14ae01bf5974
yonghwankim-dev <dragonbead95@naver.com>
2025-06-12 13:18:17 +0900

    introduce field

--- src/test/java/InvestmentCalculatorTest.java
+++ src/test/java/InvestmentCalculatorTest.java
@@ -1,17 +1,23 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InvestmentCalculatorTest {
 
+	private InvestmentCalculator calculator;
+
+	@BeforeEach
+	void setUp() {
+		calculator = new CompoundMonthlyInvestmentCalculator();
+	}
+
 	@Test
 	void created(){
-		InvestmentCalculator calculator = new CompoundMonthlyInvestmentCalculator();
 		Assertions.assertNotNull(calculator);
 	}
 
 	@Test
 	void shouldReturnPrincipal(){
-		InvestmentCalculator calculator = new CompoundMonthlyInvestmentCalculator();
 		// 월 투자 금액(원)
 		int monthlyInvestment = 1_000_000;
 		int principal = calculator.calculate(monthlyInvestment);
