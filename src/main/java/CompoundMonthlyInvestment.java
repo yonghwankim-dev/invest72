bc36ab56315cab0fdd5d3f262af87986421dfded
yonghwankim-dev <dragonbead95@naver.com>
2025-06-13 15:31:22 +0900

    remove unused class

--- src/main/java/CompoundMonthlyInvestment.java
+++
@@ -1,6 +0,0 @@
-public class CompoundMonthlyInvestment implements Investment {
-	@Override
-	public Balance calculate(InvestmentAmount investmentAmount, InvestPeriod investPeriod, InterestRate interestRate) {
-		return new CompoundBalance(investmentAmount, investPeriod, interestRate);
-	}
-}
