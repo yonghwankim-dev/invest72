21808a287ce7e85db1822b18c5eb54801bd25573
yonghwankim-dev <dragonbead95@naver.com>
2025-06-15 15:49:31 +0900

    introduce field

--- src/test/java/FixedInstallmentSavingsPlanTest.java
+++ src/test/java/FixedInstallmentSavingsPlanTest.java
@@ -1,44 +1,30 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FixedInstallmentSavingsPlanTest {
 
-	@Test
-	void created(){
-		// 월 투자 금액(원)
+	private Investment investment;
+
+	@BeforeEach
+	void setUp() {
 		InvestmentAmount investmentAmount = new MonthlyInvestmentAmount(1_000_000);
-		// 투자 기간
 		InvestPeriod investPeriod = new MonthlyInvestPeriod(12);
-		// 연 수익율
 		InterestRate annualInterestRateRate = new AnnualInterestRate(0.05);
 		TaxableFactory taxableFactory = new KoreanTaxableFactory();
-		// 세금 적용 방식
 		Taxable taxable = taxableFactory.createNonTax();
-		// 이자 계산 방식
 		Interest interest = new CompoundInterest(investmentAmount, investPeriod, annualInterestRateRate, taxable);
+		investment = new FixedInstallmentSavingsPlan(interest);
+	}
 
-		Investment investment = new FixedInstallmentSavingsPlan(interest);
-
+	@Test
+	void created(){
 		assertNotNull(investment);
 	}
 
 	@Test
 	void shouldReturnAmount(){
-		// 월 투자 금액(원)
-		InvestmentAmount investmentAmount = new MonthlyInvestmentAmount(1_000_000);
-		// 투자 기간
-		InvestPeriod investPeriod = new MonthlyInvestPeriod(12);
-		// 연 수익율
-		InterestRate annualInterestRateRate = new AnnualInterestRate(0.05);
-		TaxableFactory taxableFactory = new KoreanTaxableFactory();
-		// 세금 적용 방식
-		Taxable taxable = taxableFactory.createNonTax();
-		// 이자 계산 방식
-		Interest interest = new CompoundInterest(investmentAmount, investPeriod, annualInterestRateRate, taxable);
-
-		Investment investment = new FixedInstallmentSavingsPlan(interest);
-
 		int amount = investment.getAmount();
 
 		int expectedAmount = 12_330_017;
