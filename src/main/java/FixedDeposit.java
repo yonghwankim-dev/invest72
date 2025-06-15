c2484caf6b3722bbd2f1c071fd672cbe9d18e045
yonghwankim-dev <dragonbead95@naver.com>
2025-06-15 16:02:32 +0900

    add field in FixedDeposit

--- src/main/java/FixedDeposit.java
+++ src/main/java/FixedDeposit.java
@@ -2,6 +2,22 @@
  * 정기 예금
  */
 public class FixedDeposit implements Investment {
+
+	private final InvestmentAmount investmentAmount;
+	private final InvestPeriod investPeriod;
+	private final InterestRate interestRate;
+	private final Interest interest;
+	private final Taxable taxable;
+
+	public FixedDeposit(InvestmentAmount investmentAmount, InvestPeriod investPeriod, InterestRate interestRate,
+		Interest interest, Taxable taxable) {
+		this.investmentAmount = investmentAmount;
+		this.investPeriod = investPeriod;
+		this.interestRate = interestRate;
+		this.interest = interest;
+		this.taxable = taxable;
+	}
+
 	@Override
 	public int getAmount() {
 		return 1_051_162;
--- src/test/java/FixedDepositTest.java
+++ src/test/java/FixedDepositTest.java
@@ -1,18 +1,31 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FixedDepositTest {
-	@Test
-	void created(){
-		Investment investment = new FixedDeposit();
-		assertNotNull(investment);
+
+	private Investment investment;
+
+	@BeforeEach
+	void setUp() {
+		InvestmentAmount investmentAmount = new MonthlyInvestmentAmount(1_000_000);
+		InvestPeriod investPeriod = new MonthlyInvestPeriod(12);
+		InterestRate annualInterestRateRate = new AnnualInterestRate(0.05);
+		TaxableFactory taxableFactory = new KoreanTaxableFactory();
+		Taxable taxable = taxableFactory.createNonTax();
+		Interest interest = new CompoundInterest(investmentAmount, investPeriod, annualInterestRateRate, taxable);
+		investment = new FixedDeposit(investmentAmount, investPeriod, annualInterestRateRate, interest, taxable);
 	}
 
 	@Test
-	void shouldReturnAmount(){
-		Investment investment = new FixedDeposit();
+	void created(){
+		assertNotNull(investment);
+	}
 
+	// todo: 이 테스트는 FixedDeposit의 getAmount() 메서드가 올바른 금액을 반환하는지 확인합니다.
+	@Test
+	void shouldReturnAmount(){
 		int amount = investment.getAmount();
 
 		int expectedAmount = 1_051_162;
