5221855ebc4abb00633c39b4d9ecee1b2872d45e
yonghwankim-dev <dragonbead95@naver.com>
2025-06-16 16:48:09 +0900

    introduce field - LumpSumInvestmentAmount instance

--- src/test/java/FixedDepositAmountTest.java
+++ src/test/java/FixedDepositAmountTest.java
@@ -1,19 +1,24 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FixedDepositAmountTest {
 
+	private LumpSumInvestmentAmount investmentAmount;
+
+	@BeforeEach
+	void setUp() {
+		investmentAmount = new FixedDepositAmount(1_000_000);
+	}
+
 	@Test
 	void created(){
-		LumpSumInvestmentAmount investmentAmount = new FixedDepositAmount(1_000_000);
 		assertNotNull(investmentAmount);
 	}
 
 	@Test
 	void shouldReturnDepositAmount() {
-		LumpSumInvestmentAmount investmentAmount = new FixedDepositAmount(1_000_000);
-
 		int depositAmount = investmentAmount.getDepositAmount();
 
 		int expectedDepositAmount = 1_000_000;
