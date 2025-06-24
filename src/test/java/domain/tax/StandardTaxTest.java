4a7ac642b35bd50fd07342db6f5ed564614a5fca
yonghwankim-dev <dragonbead95@naver.com>
2025-06-24 16:26:00 +0900

    remove test methodsource

--- src/test/java/domain/tax/StandardTaxTest.java
+++ src/test/java/domain/tax/StandardTaxTest.java
@@ -2,11 +2,8 @@ package domain.tax;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.Arguments;
 
 import domain.tax.factory.KoreanTaxableFactory;
 import domain.tax.factory.TaxableFactory;
@@ -14,13 +11,6 @@ import domain.tax.factory.TaxableFactory;
 class StandardTaxTest {
 
 	private TaxableFactory taxableFactory;
-
-	public static Stream<Arguments> invalidTaxRateSource() {
-		return Stream.of(
-			Arguments.of(-0.01),
-			Arguments.of(1.0)
-		);
-	}
 
 	@BeforeEach
 	void setUp() {
