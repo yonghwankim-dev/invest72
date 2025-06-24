af84e0038f5d6ae4d1b77622991fabf158ba2bb1
yonghwankim-dev <dragonbead95@naver.com>
2025-06-24 17:12:59 +0900

    add main code

--- src/main/java/Invest72Application.java
+++ src/main/java/Invest72Application.java
@@ -1,30 +1,14 @@
-import java.text.DecimalFormat;
-
-import domain.interest_rate.AnnualInterestRate;
-import domain.interest_rate.InterestRate;
-import domain.invest_amount.InstallmentInvestmentAmount;
-import domain.invest_amount.MonthlyInstallmentInvestmentAmount;
-import domain.invest_period.InvestPeriod;
-import domain.invest_period.YearlyInvestPeriod;
-import domain.investment.Investment;
-import domain.investment.SimpleFixedInstallmentSaving;
-import domain.tax.FixedTaxRate;
-import domain.tax.StandardTax;
-import domain.tax.Taxable;
+import adapter.console.ConsoleInvestmentRunner;
+import application.CalculateInvestmentUseCase;
+import application.DefaultInvestmentFactory;
+import application.InvestmentFactory;
+import application.InvestmentUseCase;
 
 public class Invest72Application {
 	public static void main(String[] args) {
-		InstallmentInvestmentAmount investmentAmount = new MonthlyInstallmentInvestmentAmount(1_500_000);
-		InvestPeriod investPeriod = new YearlyInvestPeriod(5);
-		InterestRate interestRate = new AnnualInterestRate(0.10);
-		Taxable taxable = new StandardTax(new FixedTaxRate(0.154));
-		Investment investment = new SimpleFixedInstallmentSaving(
-			investmentAmount,
-			investPeriod,
-			interestRate,
-			taxable
-		);
-		int amount = investment.getAmount();
-		System.out.println(new DecimalFormat("#,###").format(amount) + "원");
+		InvestmentFactory investmentFactory = new DefaultInvestmentFactory();
+		InvestmentUseCase useCase = new CalculateInvestmentUseCase(investmentFactory);
+		ConsoleInvestmentRunner runner = new ConsoleInvestmentRunner(useCase, System.in);
+		runner.run();
 	}
 }
