81a8c414b72ed2d614da680d2b718c1029f8f185
yonghwankim-dev <dragonbead95@naver.com>
2025-06-24 16:17:53 +0900

    change test method name

--- src/test/java/domain/tax/FixedTaxRateTest.java
+++ src/test/java/domain/tax/FixedTaxRateTest.java
@@ -2,15 +2,30 @@ package domain.tax;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FixedTaxRateTest {
 
+	private TaxRate fixedTaxRate;
+
+	@BeforeEach
+	void setUp() {
+		fixedTaxRate = new FixedTaxRate(0.154);
+	}
+
 	@Test
 	void created() {
-		TaxRate fixedTaxRate = new FixedTaxRate(0.15);
-
 		assertNotNull(fixedTaxRate);
 	}
 
+	@Test
+	void shouldReturnTaxAmount_whenApplyToAmount() {
+		int amount = 1000;
+
+		int taxAmount = fixedTaxRate.applyTo(amount);
+
+		int expectedTaxAmount = 154;
+		assertEquals(expectedTaxAmount, taxAmount);
+	}
 }
