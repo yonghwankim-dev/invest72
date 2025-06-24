7394d67cfc7b0dc72a6ea380b143dbdfd76e082e
yonghwankim-dev <dragonbead95@naver.com>
2025-06-24 15:31:21 +0900

    add typeName

--- src/main/java/domain/type/InterestType.java
+++ src/main/java/domain/type/InterestType.java
@@ -1,6 +1,22 @@
 package domain.type;
 
 public enum InterestType {
-	SIMPLE, // 단리
-	COMPOUND // 복리
+	SIMPLE("단리"), // 단리
+	COMPOUND("복리") // 복리
+	;
+
+	private final String typeName;
+
+	InterestType(String typeName) {
+		this.typeName = typeName;
+	}
+
+	public static InterestType from(String interestType) {
+		for (InterestType type : values()) {
+			if (type.typeName.equalsIgnoreCase(interestType)) {
+				return type;
+			}
+		}
+		throw new IllegalArgumentException("Unknown interest type: " + interestType);
+	}
 }
