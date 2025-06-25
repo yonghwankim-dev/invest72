47bb02f59357e7184a8da55d2d0ff002fa9b5778
yonghwankim-dev <dragonbead95@naver.com>
2025-06-25 15:20:03 +0900

    inline varaible

--- src/test/java/adapter/console/writer/WriterBasedGuidePrinterTest.java
+++ src/test/java/adapter/console/writer/WriterBasedGuidePrinterTest.java
@@ -7,27 +7,29 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class WriterBasedGuidePrinterTest {
 
-	@Test
-	void created() {
-		OutputStream outputStream = System.out;
+	private OutputStream outputStream;
+	private GuidePrinter guidePrinter;
+
+	@BeforeEach
+	void setUp() {
+		outputStream = new ByteArrayOutputStream();
 		OutputStreamWriter writer = new OutputStreamWriter(outputStream);
 		BufferedWriter bufferedWriter = new BufferedWriter(writer);
-		GuidePrinter guidePrinter = new WriterBasedGuidePrinter(bufferedWriter);
+		guidePrinter = new WriterBasedGuidePrinter(bufferedWriter);
+	}
 
+	@Test
+	void created() {
 		assertNotNull(guidePrinter);
 	}
 
 	@Test
 	void shouldPrintFixedDepositAmountInputGuide() {
-		OutputStream outputStream = new ByteArrayOutputStream();
-		OutputStreamWriter writer = new OutputStreamWriter(outputStream);
-		BufferedWriter bufferedWriter = new BufferedWriter(writer);
-		GuidePrinter guidePrinter = new WriterBasedGuidePrinter(bufferedWriter);
-
 		guidePrinter.printFixedDepositAmountInputGuide();
 
 		String output = outputStream.toString();
