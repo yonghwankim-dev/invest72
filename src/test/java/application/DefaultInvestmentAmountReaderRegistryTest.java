59ed48ab962211b79b995ce1b7f8329544eebdec
yonghwankim-dev <dragonbead95@naver.com>
2025-06-25 14:33:12 +0900

    add test

--- src/test/java/application/DefaultInvestmentAmountReaderRegistryTest.java
+++ src/test/java/application/DefaultInvestmentAmountReaderRegistryTest.java
@@ -2,7 +2,11 @@ package application;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import adapter.console.reader.InvestmentAmountReader;
 
 class DefaultInvestmentAmountReaderRegistryTest {
 
@@ -12,4 +16,13 @@ class DefaultInvestmentAmountReaderRegistryTest {
 		assertNotNull(registry);
 	}
 
+	@Test
+	void getReaders() {
+		InvestmentAmountReaderRegistry registry = new DefaultInvestmentAmountReaderRegistry(System.out);
+
+		List<InvestmentAmountReader> readers = registry.getReaders();
+
+		assertNotNull(readers);
+		assertEquals(readers.size(), 2);
+	}
 }
