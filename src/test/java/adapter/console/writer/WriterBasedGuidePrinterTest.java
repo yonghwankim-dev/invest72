d63354703cab86e5b78a91a37534bae59a07444c
yonghwankim-dev <dragonbead95@naver.com>
2025-06-25 15:36:13 +0900

    add test - printInstallmentInvestmentInputGuide

--- src/test/java/adapter/console/writer/WriterBasedGuidePrinterTest.java
+++ src/test/java/adapter/console/writer/WriterBasedGuidePrinterTest.java
@@ -35,4 +35,29 @@ class WriterBasedGuidePrinterTest {
 		String output = outputStream.toString();
 		assertTrue(output.contains("예치 금액(원)을 입력하세요"));
 	}
+
+	@Test
+	void shouldPrintInstallmentInvestmentInputGuide() {
+		guidePrinter.printInstallmentInvestmentInputGuide();
+
+		String output = outputStream.toString();
+		assertEquals(getInstallmentInvestmentInputGuide(), output);
+	}
+
+	private String getInstallmentInvestmentInputGuide() {
+		return "\uD83D\uDCB0 투자 기간 단위와 금액을 한 줄로 입력해주세요.\n"
+			+ "\n"
+			+ "\uD83D\uDCDD 형식:\n"
+			+ "[단위] [투자금액]\n"
+			+ "\n"
+			+ "\uD83D\uDCCC 단위 예시:\n"
+			+ "- \"월\" → 적금 (매월 납입 금액)\n"
+			+ "- \"년\" → 적금 (매년 납입 금액)\n"
+			+ "\n"
+			+ "\uD83D\uDCCC 예시 입력:\n"
+			+ "- 월 1000000\n"
+			+ "- 년 5000000\n"
+			+ "\n"
+			+ "\uD83D\uDC49 입력: \n";
+	}
 }
