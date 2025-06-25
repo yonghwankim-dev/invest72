9de84c885bec9950fece3a7c35661e9f0c6289f2
yonghwankim-dev <dragonbead95@naver.com>
2025-06-25 13:28:34 +0900

    feat FixedDepositAmountReader read method code

--- src/main/java/adapter/console/reader/FixedDepositAmountReader.java
+++ src/main/java/adapter/console/reader/FixedDepositAmountReader.java
@@ -2,12 +2,23 @@ package adapter.console.reader;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintStream;
 
+import domain.invest_amount.FixedDepositAmount;
 import domain.invest_amount.InvestmentAmount;
 
 public class FixedDepositAmountReader implements InvestmentAmountReader {
+
+	private final PrintStream out;
+
+	public FixedDepositAmountReader(PrintStream out) {
+		this.out = out;
+	}
+
 	@Override
 	public InvestmentAmount read(BufferedReader reader) throws IOException {
-		return null;
+		out.print("예치 금액(원)을 입력하세요: ");
+		int amount = Integer.parseInt(reader.readLine());
+		return new FixedDepositAmount(amount);
 	}
 }
--- src/test/java/adapter/console/reader/FixedDepositAmountReaderTest.java
+++ src/test/java/adapter/console/reader/FixedDepositAmountReaderTest.java
@@ -2,14 +2,38 @@ package adapter.console.reader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 import org.junit.jupiter.api.Test;
+
+import domain.invest_amount.InvestmentAmount;
 
 class FixedDepositAmountReaderTest {
 
 	@Test
 	void created() {
-		InvestmentAmountReader reader = new FixedDepositAmountReader();
+		InvestmentAmountReader reader = new FixedDepositAmountReader(System.out);
 
 		assertNotNull(reader);
+	}
+
+	@Test
+	void shouldReturnFixedDepositAmount_whenReaderIsFixedDepositAmountReaderType() throws IOException {
+		InvestmentAmountReader reader = new FixedDepositAmountReader(System.out);
+
+		String input = String.join(System.lineSeparator(),
+			"1000000"
+		);
+		InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+		InvestmentAmount investmentAmount = reader.read(bufferedReader);
+
+		assertInstanceOf(domain.invest_amount.FixedDepositAmount.class, investmentAmount);
 	}
 }
