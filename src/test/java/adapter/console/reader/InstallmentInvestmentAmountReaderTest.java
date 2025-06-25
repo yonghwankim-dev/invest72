26ebb980985c3375179222061d6a4d288d23df60
yonghwankim-dev <dragonbead95@naver.com>
2025-06-25 13:36:20 +0900

    add test - read

--- src/test/java/adapter/console/reader/InstallmentInvestmentAmountReaderTest.java
+++ src/test/java/adapter/console/reader/InstallmentInvestmentAmountReaderTest.java
@@ -2,7 +2,17 @@ package adapter.console.reader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 import org.junit.jupiter.api.Test;
+
+import domain.invest_amount.InvestmentAmount;
+import domain.invest_amount.MonthlyInstallmentInvestmentAmount;
 
 class InstallmentInvestmentAmountReaderTest {
 
@@ -11,5 +21,21 @@ class InstallmentInvestmentAmountReaderTest {
 		InvestmentAmountReader reader = new InstallmentInvestmentAmountReader(System.out);
 
 		assertNotNull(reader);
+	}
+
+	@Test
+	void shouldReturnInstallmentInvestmentAmount_whenReaderIsInstallmentInvestmentAmountReaderType() throws
+		IOException {
+		InvestmentAmountReader reader = new InstallmentInvestmentAmountReader(System.out);
+
+		String input = String.join(System.lineSeparator(),
+			"월 1000000"
+		);
+		InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+		InvestmentAmount investmentAmount = reader.read(bufferedReader);
+
+		assertInstanceOf(MonthlyInstallmentInvestmentAmount.class, investmentAmount);
 	}
 }
