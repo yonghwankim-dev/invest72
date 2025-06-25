c5a9cac6b594407035cb271d95646cfcb4960952
yonghwankim-dev <dragonbead95@naver.com>
2025-06-25 14:12:35 +0900

    introduce field

--- src/test/java/adapter/console/ConsoleInvestmentRunnerTest.java
+++ src/test/java/adapter/console/ConsoleInvestmentRunnerTest.java
@@ -9,6 +9,7 @@ import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import adapter.console.reader.ConsoleInvestmentAmountReaderDelegator;
@@ -23,18 +24,31 @@ import application.InvestmentUseCase;
 
 class ConsoleInvestmentRunnerTest {
 
-	@Test
-	void created() {
+	private InvestmentUseCase useCase;
+	private ByteArrayOutputStream outputStream;
+	private PrintStream printStream;
+	private InvestmentAmountReaderDelegator investmentAmountReaderDelegator;
+	private InputStream inputStream;
+
+	@BeforeEach
+	void setUp() {
 		InvestmentFactory investmentFactory = new DefaultInvestmentFactory();
-		InvestmentUseCase useCase = new CalculateInvestmentUseCase(investmentFactory);
+		useCase = new CalculateInvestmentUseCase(investmentFactory);
 		PrintStream out = System.out;
 		List<InvestmentAmountReader> investmentAmountReaders = List.of(
 			new FixedDepositAmountReader(out),
 			new InstallmentInvestmentAmountReader(out)
 		);
-		InvestmentAmountReaderDelegator investmentAmountReaderDelegator = new ConsoleInvestmentAmountReaderDelegator(
+		inputStream = System.in;
+		outputStream = new ByteArrayOutputStream();
+		printStream = new PrintStream(outputStream);
+		investmentAmountReaderDelegator = new ConsoleInvestmentAmountReaderDelegator(
 			investmentAmountReaders);
-		ConsoleInvestmentRunner runner = new ConsoleInvestmentRunner(useCase, System.in, out,
+	}
+
+	@Test
+	void created() {
+		ConsoleInvestmentRunner runner = new ConsoleInvestmentRunner(useCase, inputStream, printStream,
 			investmentAmountReaderDelegator);
 
 		assertNotNull(runner);
@@ -52,19 +66,7 @@ class ConsoleInvestmentRunnerTest {
 			"비과세",
 			"0"
 		);
-		InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
-
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		PrintStream printStream = new PrintStream(outputStream);
-
-		InvestmentFactory investmentFactory = new DefaultInvestmentFactory();
-		InvestmentUseCase useCase = new CalculateInvestmentUseCase(investmentFactory);
-		List<InvestmentAmountReader> investmentAmountReaders = List.of(
-			new FixedDepositAmountReader(printStream),
-			new InstallmentInvestmentAmountReader(printStream)
-		);
-		InvestmentAmountReaderDelegator investmentAmountReaderDelegator = new ConsoleInvestmentAmountReaderDelegator(
-			investmentAmountReaders);
+		inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
 		ConsoleInvestmentRunner runner = new ConsoleInvestmentRunner(useCase, inputStream, printStream,
 			investmentAmountReaderDelegator);
 
