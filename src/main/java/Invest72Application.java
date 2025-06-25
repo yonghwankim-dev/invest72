906e4921a141d4cd6bf7c060dd6b67d536fe3ca0
yonghwankim-dev <dragonbead95@naver.com>
2025-06-25 14:51:31 +0900

    refactor intrudoce InputStream variable

--- src/main/java/Invest72Application.java
+++ src/main/java/Invest72Application.java
@@ -1,3 +1,4 @@
+import java.io.InputStream;
 import java.io.PrintStream;
 
 import adapter.console.ConsoleInvestmentRunner;
@@ -14,12 +15,12 @@ public class Invest72Application {
 	public static void main(String[] args) {
 		InvestmentFactory investmentFactory = new DefaultInvestmentFactory();
 		InvestmentUseCase useCase = new CalculateInvestmentUseCase(investmentFactory);
+		InputStream in = System.in;
 		PrintStream out = System.out;
 		InvestmentAmountReaderRegistry investmentAmountReaderRegistry = new DefaultInvestmentAmountReaderRegistry(out);
 		InvestmentAmountReaderDelegator investmentAmountReaderDelegator = new ConsoleInvestmentAmountReaderDelegator(
 			investmentAmountReaderRegistry);
-		ConsoleInvestmentRunner runner = new ConsoleInvestmentRunner(useCase, System.in, out,
-			investmentAmountReaderDelegator);
+		ConsoleInvestmentRunner runner = new ConsoleInvestmentRunner(useCase, in, out, investmentAmountReaderDelegator);
 		runner.run();
 	}
 }
