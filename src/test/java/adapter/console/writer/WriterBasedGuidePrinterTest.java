bf59ad3a2ad716d44dc90f9fc02c1a7874eff5e3
yonghwankim-dev <dragonbead95@naver.com>
2025-06-25 15:18:16 +0900

    add test - printFixedDepositAmountInputGuide method

--- src/test/java/adapter/console/writer/WriterBasedGuidePrinterTest.java
+++ src/test/java/adapter/console/writer/WriterBasedGuidePrinterTest.java
@@ -3,6 +3,7 @@ package adapter.console.writer;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
@@ -18,5 +19,18 @@ class WriterBasedGuidePrinterTest {
 		GuidePrinter guidePrinter = new WriterBasedGuidePrinter(bufferedWriter);
 
 		assertNotNull(guidePrinter);
+	}
+
+	@Test
+	void shouldPrintFixedDepositAmountInputGuide() {
+		OutputStream outputStream = new ByteArrayOutputStream();
+		OutputStreamWriter writer = new OutputStreamWriter(outputStream);
+		BufferedWriter bufferedWriter = new BufferedWriter(writer);
+		GuidePrinter guidePrinter = new WriterBasedGuidePrinter(bufferedWriter);
+
+		guidePrinter.printFixedDepositAmountInputGuide();
+
+		String output = outputStream.toString();
+		assertTrue(output.contains("예치 금액(원)을 입력하세요"));
 	}
 }
