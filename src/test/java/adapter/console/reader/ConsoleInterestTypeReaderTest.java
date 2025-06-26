aff106199ac951b05d809460d000fb3f7cb46ca1
yonghwankim-dev <dragonbead95@naver.com>
2025-06-26 13:05:13 +0900

    add test - ConsoleInterestTypeReader created

---
+++ src/test/java/adapter/console/reader/ConsoleInterestTypeReaderTest.java
@@ -0,0 +1,25 @@
+package adapter.console.reader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.Test;
+
+import adapter.console.writer.GuidePrinter;
+import adapter.console.writer.WriterBasedGuidePrinter;
+
+class ConsoleInterestTypeReaderTest {
+	@Test
+	void created() {
+		PrintStream out = System.out;
+		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out);
+		BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+		GuidePrinter guidePrinter = new WriterBasedGuidePrinter(bufferedWriter);
+		InterestTypeReader interestTypeReader = new ConsoleInterestTypeReader(guidePrinter);
+
+		assertNotNull(interestTypeReader);
+	}
+}
