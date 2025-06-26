30e83f6efc66e33b55bd8a931897d7b0f22b7cff
yonghwankim-dev <dragonbead95@naver.com>
2025-06-26 13:10:10 +0900

    add test - read interest type

--- src/test/java/adapter/console/reader/ConsoleInterestTypeReaderTest.java
+++ src/test/java/adapter/console/reader/ConsoleInterestTypeReaderTest.java
@@ -2,9 +2,11 @@ package adapter.console.reader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
+import java.io.StringReader;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,4 +32,21 @@ class ConsoleInterestTypeReaderTest {
 		assertNotNull(interestTypeReader);
 	}
 
+	@Test
+	void shouldReturnInterestType_whenTextIsSimple() throws Exception {
+		BufferedReader reader = new BufferedReader(new StringReader("단리"));
+
+		String interestType = interestTypeReader.read(reader);
+
+		assertEquals("단리", interestType);
+	}
+
+	@Test
+	void shouldReturnInterestType_whenTextIsCompound() throws Exception {
+		BufferedReader reader = new BufferedReader(new StringReader("복리"));
+
+		String interestType = interestTypeReader.read(reader);
+
+		assertEquals("복리", interestType);
+	}
 }
