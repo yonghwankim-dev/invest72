7f1e736ef00f2ded4688e442c5850fab7b25b2a6
yonghwankim-dev <dragonbead95@naver.com>
2025-06-26 13:05:45 +0900

    introduce field - ConsoleInterestTypeReader

--- src/test/java/adapter/console/reader/ConsoleInterestTypeReaderTest.java
+++ src/test/java/adapter/console/reader/ConsoleInterestTypeReaderTest.java
@@ -6,20 +6,28 @@ import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import adapter.console.writer.GuidePrinter;
 import adapter.console.writer.WriterBasedGuidePrinter;
 
 class ConsoleInterestTypeReaderTest {
-	@Test
-	void created() {
+
+	private InterestTypeReader interestTypeReader;
+
+	@BeforeEach
+	void setUp() {
 		PrintStream out = System.out;
 		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out);
 		BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
 		GuidePrinter guidePrinter = new WriterBasedGuidePrinter(bufferedWriter);
-		InterestTypeReader interestTypeReader = new ConsoleInterestTypeReader(guidePrinter);
+		interestTypeReader = new ConsoleInterestTypeReader(guidePrinter);
+	}
 
+	@Test
+	void created() {
 		assertNotNull(interestTypeReader);
 	}
+
 }
