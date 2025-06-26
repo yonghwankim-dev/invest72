749fe8595ab72de4fa68fcd6fb565487c21378ba
yonghwankim-dev <dragonbead95@naver.com>
2025-06-26 13:53:54 +0900

    introduce field - COnsoleTaxTypeReader

---
+++ src/test/java/adapter/console/reader/ConsoleTaxRateReaderTest.java
@@ -0,0 +1,32 @@
+package adapter.console.reader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import adapter.console.writer.GuidePrinter;
+import adapter.console.writer.WriterBasedGuidePrinter;
+
+class ConsoleTaxRateReaderTest {
+
+	private TaxTypeReader reader;
+
+	@BeforeEach
+	void setUp() {
+		PrintStream out = System.out;
+		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out);
+		BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+		GuidePrinter guidePrinter = new WriterBasedGuidePrinter(bufferedWriter);
+		reader = new ConsoleTaxTypeReader(guidePrinter);
+	}
+
+	@Test
+	void created() {
+		assertNotNull(reader);
+	}
+}
