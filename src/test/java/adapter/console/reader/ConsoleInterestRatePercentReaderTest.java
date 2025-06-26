4159075ee3fbcf2903bb6fab40b476752048a7c1
yonghwankim-dev <dragonbead95@naver.com>
2025-06-26 13:21:40 +0900

    add test - InterestRatePercentReader created

---
+++ src/test/java/adapter/console/reader/ConsoleInterestRatePercentReaderTest.java
@@ -0,0 +1,26 @@
+package adapter.console.reader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.Test;
+
+import adapter.console.writer.GuidePrinter;
+import adapter.console.writer.WriterBasedGuidePrinter;
+
+class ConsoleInterestRatePercentReaderTest {
+
+	@Test
+	void created() {
+		PrintStream out = System.out;
+		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out);
+		BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+		GuidePrinter guidePrinter = new WriterBasedGuidePrinter(bufferedWriter);
+		InterestRatePercentReader reader = new ConsoleInterestRatePercentReader(guidePrinter);
+
+		assertNotNull(reader);
+	}
+}
