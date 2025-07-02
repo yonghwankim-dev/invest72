d07971fed7ac808c96c55cbe23898497af96b881
yonghwankim-dev <dragonbead95@naver.com>
2025-07-02 16:32:08 +0900

    extract method

--- src/main/java/adapter/console/reader/CalculateInvestmentReaderDelegator.java
+++ src/main/java/adapter/console/reader/CalculateInvestmentReaderDelegator.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import application.CalculateInvestmentRequest;
 import application.InvestmentRequestBuilder;
+import domain.type.InvestmentType;
 
 public class CalculateInvestmentReaderDelegator implements InvestmentReaderDelegator {
 	private final InvestReader investReader;
@@ -19,7 +20,7 @@ public class CalculateInvestmentReaderDelegator implements InvestmentReaderDeleg
 	@Override
 	public CalculateInvestmentRequest readInvestmentRequest() throws IOException {
 		String investmentType = investReader.readInvestmentType();
-		String investmentAmountLine = investReader.readInstallmentSavingAmount();
+		String investmentAmount = readInvestmentAmount(investmentType);
 		String periodType = investReader.readPeriodType();
 		int periodValue = investReader.readPeriodValue();
 		String interestType = investReader.readInterestType();
@@ -29,7 +30,7 @@ public class CalculateInvestmentReaderDelegator implements InvestmentReaderDeleg
 
 		CalculateInvestmentRequest.CalculateInvestmentRequestBuilder builder = requestBuilder.calculateInvestmentRequestBuilder();
 		return builder.type(investmentType)
-			.amount(investmentAmountLine)
+			.amount(investmentAmount)
 			.periodType(periodType)
 			.periodValue(periodValue)
 			.interestType(interestType)
@@ -37,5 +38,14 @@ public class CalculateInvestmentReaderDelegator implements InvestmentReaderDeleg
 			.taxType(taxType)
 			.taxRate(taxRate)
 			.build();
+	}
+
+	private String readInvestmentAmount(String investmentType) throws IOException {
+		if (InvestmentType.FIXED_DEPOSIT.getTypeName().equals(investmentType)) {
+			return investReader.readFixedDepositAmount();
+		} else if (InvestmentType.INSTALLMENT_SAVING.getTypeName().equals(investmentType)) {
+			return investReader.readInstallmentSavingAmount();
+		}
+		throw new IllegalArgumentException("지원하지 않는 투자 유형입니다: " + investmentType);
 	}
 }
