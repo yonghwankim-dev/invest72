d630518f4d6d738c0077af9fe4d894248bb2d786
yonghwankim-dev <dragonbead95@naver.com>
2025-07-03 12:02:10 +0900

    remove unused code

--- src/main/java/Invest72Application.java
+++ src/main/java/Invest72Application.java
@@ -8,41 +8,21 @@ import java.util.Map;
 
 import adapter.InvestmentApplicationRunner;
 import adapter.console.CalculateInvestmentRunner;
-import adapter.console.reader.AnnualInterestRateReader;
 import adapter.console.reader.BufferedReaderBasedInvestReader;
 import adapter.console.reader.CalculateInvestmentReaderDelegator;
-import adapter.console.reader.ConsoleInterestTypeReader;
 import adapter.console.reader.FixedDepositAmountReaderStrategy;
-import adapter.console.reader.FixedTaxRateReader;
 import adapter.console.reader.InstallmentSavingAmountReaderStrategy;
-import adapter.console.reader.InterestRatePercentReader;
-import adapter.console.reader.InterestTypeReader;
 import adapter.console.reader.InvestReader;
-import adapter.console.reader.InvestmentAmountReaderDelegator;
 import adapter.console.reader.InvestmentAmountReaderStrategy;
 import adapter.console.reader.InvestmentAmountReaderStrategyRegistry;
 import adapter.console.reader.InvestmentReaderDelegator;
-import adapter.console.reader.InvestmentTypeInputReader;
-import adapter.console.reader.InvestmentTypeReader;
 import adapter.console.reader.MapBasedInvestmentAmountReaderStrategyRegistry;
-import adapter.console.reader.PeriodInputReader;
-import adapter.console.reader.PeriodReader;
-import adapter.console.reader.PeriodTypeInputReader;
-import adapter.console.reader.PeriodTypeReader;
-import adapter.console.reader.RegistryBasedInvestmentAmountDelegator;
-import adapter.console.reader.TaxRateReader;
-import adapter.console.reader.TaxTypeInputReader;
-import adapter.console.reader.TaxTypeReader;
 import adapter.console.writer.GuidePrinter;
 import adapter.console.writer.WriterBasedGuidePrinter;
-import application.CalculateInvestmentUseCase;
-import application.DefaultInvestmentAmountReaderRegistry;
 import application.DefaultInvestmentFactory;
 import application.DefaultInvestmentRequestBuilder;
-import application.InvestmentAmountReaderRegistry;
 import application.InvestmentFactory;
 import application.InvestmentRequestBuilder;
-import application.InvestmentUseCase;
 import application.InvestmentUseCaseFactory;
 import application.KoreanStringBasedTaxableResolver;
 import application.TaxableResolver;
@@ -59,19 +39,13 @@ public class Invest72Application {
 
 	private static InvestmentApplicationRunner createConsoleBasedInvestmentCalculateRunner() {
 		InvestmentFactory investmentFactory = new DefaultInvestmentFactory();
-		InvestmentUseCase useCase = new CalculateInvestmentUseCase(investmentFactory);
 		UseCaseFactory useCaseFactory = new InvestmentUseCaseFactory(investmentFactory);
 		InputStream in = System.in;
 		PrintStream out = System.out;
 		PrintStream err = System.err;
 
 		GuidePrinter guidPrinter = createGuidPrinter(out);
-		InvestmentAmountReaderRegistry investmentAmountReaderRegistry = createInvestmentAmountReaderRegistry(
-			guidPrinter);
-		InvestmentTypeReader investmentTypeReaderDelegator = createInvestTypeReaderDelegator(
-			guidPrinter);
-		InvestmentReaderDelegator investmentReaderDelegator = createInvestmentReaderDelegator(
-			investmentAmountReaderRegistry, guidPrinter, investmentTypeReaderDelegator, in);
+		InvestmentReaderDelegator investmentReaderDelegator = createInvestmentReaderDelegator(guidPrinter, in);
 		TaxableFactory taxableFactory = new KoreanTaxableFactory();
 		TaxableResolver taxableResolver = new KoreanStringBasedTaxableResolver(taxableFactory);
 		return new CalculateInvestmentRunner(
@@ -84,18 +58,8 @@ public class Invest72Application {
 	}
 
 	private static InvestmentReaderDelegator createInvestmentReaderDelegator(
-		InvestmentAmountReaderRegistry investmentAmountReaderRegistry,
 		GuidePrinter guidPrinter,
-		InvestmentTypeReader investmentTypeReaderDelegator,
 		InputStream in) {
-		InvestmentAmountReaderDelegator investmentAmountReaderDelegator = createInvestmentAmountReaderDelegator(
-			investmentAmountReaderRegistry);
-		PeriodTypeReader periodTypeReader = createPeriodTypeReaderDelegator(guidPrinter);
-		PeriodReader periodReader = new PeriodInputReader(guidPrinter);
-		InterestTypeReader interestTypeReader = new ConsoleInterestTypeReader(guidPrinter);
-		InterestRatePercentReader interestRatePercentReader = new AnnualInterestRateReader(guidPrinter);
-		TaxTypeReader taxTypeReader = new TaxTypeInputReader(guidPrinter);
-		TaxRateReader taxRateReader = new FixedTaxRateReader(guidPrinter);
 		InvestmentRequestBuilder requestBuilder = new DefaultInvestmentRequestBuilder();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 		InvestReader investReader = new BufferedReaderBasedInvestReader(reader, guidPrinter);
@@ -114,23 +78,5 @@ public class Invest72Application {
 		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out);
 		BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
 		return new WriterBasedGuidePrinter(bufferedWriter);
-	}
-
-	private static InvestmentAmountReaderRegistry createInvestmentAmountReaderRegistry(GuidePrinter guidePrinter) {
-		return new DefaultInvestmentAmountReaderRegistry(guidePrinter);
-	}
-
-	private static InvestmentTypeReader createInvestTypeReaderDelegator(GuidePrinter guidPrinter) {
-		return new InvestmentTypeInputReader(guidPrinter);
-	}
-
-	private static InvestmentAmountReaderDelegator createInvestmentAmountReaderDelegator(
-		InvestmentAmountReaderRegistry investmentAmountReaderRegistry) {
-		return new RegistryBasedInvestmentAmountDelegator(
-			investmentAmountReaderRegistry);
-	}
-
-	private static PeriodTypeInputReader createPeriodTypeReaderDelegator(GuidePrinter guidPrinter) {
-		return new PeriodTypeInputReader(guidPrinter);
 	}
 }
