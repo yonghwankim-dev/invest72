fe928ff3da041308970d55f2120780d5d33223b3
yonghwankim-dev <dragonbead95@naver.com>
2025-07-07 15:13:21 +0900

    add genertic type

--- src/main/java/application/factory/InvestmentUseCaseFactory.java
+++ src/main/java/application/factory/InvestmentUseCaseFactory.java
@@ -2,12 +2,13 @@ package application.factory;
 
 import application.usecase.CalculateInvestmentUseCase;
 import application.usecase.InvestmentUseCase;
+import domain.investment.Investment;
 
 public class InvestmentUseCaseFactory implements UseCaseFactory {
 
-	private final InvestmentFactory investmentFactory;
+	private final InvestmentFactory<Investment> investmentFactory;
 
-	public InvestmentUseCaseFactory(InvestmentFactory investmentFactory) {
+	public InvestmentUseCaseFactory(InvestmentFactory<Investment> investmentFactory) {
 		this.investmentFactory = investmentFactory;
 	}
 
