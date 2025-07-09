f87ce486d5b9edc1dec2450aca21b67a857f57ac
yonghwankim-dev <dragonbead95@naver.com>
2025-07-09 14:03:01 +0900

    fix comment

--- src/main/java/domain/investment/MonthlyInvestment.java
+++ src/main/java/domain/investment/MonthlyInvestment.java
@@ -18,7 +18,7 @@ public interface MonthlyInvestment {
 	int getAccumulatedInterest(int month);
 
 	/**
-	 * month 회차에 해당하는 세금 금액을 반환합니다.
+	 * month 회차에 해당하는 누적 세금 금액을 반환합니다.
 	 *
 	 * @param month 회차 (1부터 시작)
 	 * @return 세금 금액
