b0439b3c9af0be2795ce9fd580502baf6176ab19
yonghwankim-dev <dragonbead95@naver.com>
2025-07-09 16:24:40 +0900

    change method signiture - parse, String to File

--- src/test/java/InvestmentJsonParserTest.java
+++ src/test/java/InvestmentJsonParserTest.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.IOException;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,6 +12,13 @@ import application.builder.InvestmentRequestBuilder;
 import application.request.CalculateInvestmentRequest;
 
 class InvestmentJsonParserTest {
+
+	private ObjectMapper objectMapper;
+
+	@BeforeEach
+	void setUp() {
+		objectMapper = new ObjectMapper();
+	}
 
 	@Test
 	void parse_shouldReturnInvestmentRequest() {
@@ -25,14 +33,13 @@ class InvestmentJsonParserTest {
 			.taxType("일반과세")
 			.taxRate(0.154)
 			.build();
+		File file = new File("src/test/resources/test_input1.json");
 
-		CalculateInvestmentRequest request = parse("src/test/resources/test_input1.json");
+		CalculateInvestmentRequest request = parse(file);
 		Assertions.assertEquals(expectedInvestmentRequest, request);
 	}
 
-	private CalculateInvestmentRequest parse(String path) {
-		File file = new File(path);
-		ObjectMapper objectMapper = new ObjectMapper();
+	private CalculateInvestmentRequest parse(File file) {
 		try {
 			return objectMapper.readValue(file, CalculateInvestmentRequest.class);
 		} catch (IOException e) {
