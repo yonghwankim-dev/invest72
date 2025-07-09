94c43c62de6ad5029ec12d583090649c1b96baeb
yonghwankim-dev <dragonbead95@naver.com>
2025-07-09 16:19:21 +0900

    build jackson library

---
+++ src/test/java/InvestmentJsonParserTest.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import application.builder.DefaultInvestmentRequestBuilder;
+import application.builder.InvestmentRequestBuilder;
+import application.request.CalculateInvestmentRequest;
+
+class InvestmentJsonParserTest {
+
+	@Test
+	void parse_shouldReturnInvestmentRequest() {
+		InvestmentRequestBuilder builder = new DefaultInvestmentRequestBuilder();
+		CalculateInvestmentRequest expectedInvestmentRequest = builder.calculateInvestmentRequestBuilder()
+			.type("예금")
+			.amount("1000000")
+			.periodType("년")
+			.periodValue(1)
+			.interestType("단리")
+			.interestRate(0.05)
+			.taxType("일반과세")
+			.taxRate(0.154)
+			.build();
+
+		CalculateInvestmentRequest request = parse("src/test/resources/test_input1.json");
+		String type = request.type();
+		Assertions.assertEquals(expectedInvestmentRequest.type(), type);
+	}
+
+	private CalculateInvestmentRequest parse(String path) {
+		File file = new File(path);
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			return objectMapper.readValue(file, CalculateInvestmentRequest.class);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
