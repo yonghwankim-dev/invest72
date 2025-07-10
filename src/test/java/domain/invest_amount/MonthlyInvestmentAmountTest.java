04d106176f583155d3ae8266d82b4334f01fc88c
yonghwankim-dev <dragonbead95@naver.com>
2025-07-10 13:18:26 +0900

    introduce field - InvestmentAmount, InterestRate

--- src/test/java/domain/invest_amount/MonthlyInvestmentAmountTest.java
+++ src/test/java/domain/invest_amount/MonthlyInvestmentAmountTest.java
@@ -2,6 +2,7 @@ package domain.invest_amount;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import domain.interest_rate.AnnualInterestRate;
@@ -9,17 +10,22 @@ import domain.interest_rate.InterestRate;
 
 class MonthlyInvestmentAmountTest {
 
+	private InvestmentAmount investmentAmount;
+	private InterestRate interestRate;
+
+	@BeforeEach
+	void setUp() {
+		investmentAmount = new MonthlyInvestmentAmount(1_000_000);
+		interestRate = new AnnualInterestRate(0.05);
+	}
+
 	@Test
 	void created() {
-		InvestmentAmount investmentAmount = new MonthlyInvestmentAmount(1_000_000);
 		assertNotNull(investmentAmount);
 	}
 
 	@Test
 	void calAnnualInterest_shouldReturnAnnualInterest() {
-		InvestmentAmount investmentAmount = new MonthlyInvestmentAmount(1_000_000);
-		InterestRate interestRate = new AnnualInterestRate(0.05);
-
 		double annualInterest = investmentAmount.calAnnualInterest(interestRate);
 
 		double expectedAnnualInterest = 50_000;
