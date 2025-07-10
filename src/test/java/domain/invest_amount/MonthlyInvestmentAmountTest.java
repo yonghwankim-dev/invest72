b73a1a61d8ed3bcd43e8ad94efe49c532d55baf4
yonghwankim-dev <dragonbead95@naver.com>
2025-07-10 13:15:24 +0900

    update calAnnualInterest

--- src/test/java/domain/invest_amount/MonthlyInvestmentAmountTest.java
+++ src/test/java/domain/invest_amount/MonthlyInvestmentAmountTest.java
@@ -4,11 +4,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import domain.interest_rate.AnnualInterestRate;
+import domain.interest_rate.InterestRate;
+
 class MonthlyInvestmentAmountTest {
 
 	@Test
 	void created() {
 		InvestmentAmount investmentAmount = new MonthlyInvestmentAmount(1_000_000);
 		assertNotNull(investmentAmount);
+	}
+
+	@Test
+	void calAnnualInterest_shouldReturnAnnualInterest() {
+		InvestmentAmount investmentAmount = new MonthlyInvestmentAmount(1_000_000);
+		InterestRate interestRate = new AnnualInterestRate(0.05);
+
+		double annualInterest = investmentAmount.calAnnualInterest(interestRate);
+
+		double expectedAnnualInterest = 50_000;
+		assertEquals(expectedAnnualInterest, annualInterest, 0.01);
 	}
 }
