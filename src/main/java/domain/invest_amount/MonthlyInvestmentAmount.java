463e4300ea2320349f6932561b13b2d24931097e
yonghwankim-dev <dragonbead95@naver.com>
2025-07-10 13:07:48 +0900

    add imple class - MonthlyInvestmentAmount

---
+++ src/main/java/domain/invest_amount/MonthlyInvestmentAmount.java
@@ -0,0 +1,25 @@
+package domain.invest_amount;
+
+import domain.interest_rate.InterestRate;
+
+public class MonthlyInvestmentAmount implements InvestmentAmount {
+
+	private final int amount;
+
+	public MonthlyInvestmentAmount(int amount) {
+		this.amount = amount;
+		if (amount <= 0) {
+			throw new IllegalArgumentException("월 투자 금액은 0보다 커야 합니다.");
+		}
+	}
+
+	@Override
+	public double calAnnualInterest(InterestRate interestRate) {
+		return 0;
+	}
+
+	@Override
+	public int calMonthlyInterest(InterestRate interestRate) {
+		return 0;
+	}
+}
