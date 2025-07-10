4042668b2514044d2f7eae6aa140c803e76a2b20
yonghwankim-dev <dragonbead95@naver.com>
2025-07-10 14:07:56 +0900

    change method signiture - int to TargetDurationCalculator
    - TargetDurationCaluclator의 구현체 클래스가 내부적으로 값 검증을 수행하여 조건문을 제거함

--- src/main/java/application/usecase/MonthlyTargetAchievementUseCase.java
+++ src/main/java/application/usecase/MonthlyTargetAchievementUseCase.java
@@ -3,6 +3,7 @@ package application.usecase;
 import java.time.LocalDate;
 
 import application.time.DateProvider;
+import domain.invest_amount.TargetDurationCalculator;
 
 /**
  * 초기 자본금과 월 투자금액을 기반으로 목표 달성 금액을 도달하는데 걸리는 시간을 계산하는 유스케이스입니다.
@@ -16,17 +17,11 @@ public class MonthlyTargetAchievementUseCase implements TargetAchievementUseCase
 	}
 
 	@Override
-	public LocalDate calTargetAchievement(int targetAmount, int monthlyInvestmentAmount) {
+	public LocalDate calTargetAchievement(int targetAmount, TargetDurationCalculator monthlyInvestmentAmount) {
 		if (targetAmount <= 0) {
 			throw new IllegalArgumentException("목표 달성 금액은 0보다 커야 합니다.");
 		}
-		if (monthlyInvestmentAmount <= 0) {
-			throw new IllegalArgumentException("월 투자 금액은 0보다 커야 합니다.");
-		}
-		if (monthlyInvestmentAmount >= targetAmount) {
-			return dateProvider.now();
-		}
-		int months = (targetAmount / monthlyInvestmentAmount) - 1;
+		int months = monthlyInvestmentAmount.calMonthsToReachTarget(targetAmount);
 		return dateProvider.now().plusMonths(months);
 	}
 }
