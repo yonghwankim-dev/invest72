58c2b8052bf5605784fba21e601922a9cc617799
yonghwankim-dev <dragonbead95@naver.com>
2025-07-11 14:39:40 +0900

    add output assert

--- src/test/java/adapter/console/CalculateTargetAchievementRunnerTest.java
+++ src/test/java/adapter/console/CalculateTargetAchievementRunnerTest.java
@@ -1,27 +1,37 @@
 package adapter.console;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import adapter.InvestmentApplicationRunner;
 
 class CalculateTargetAchievementRunnerTest {
 
+	private InvestmentApplicationRunner runner;
+	private OutputStream outputStream;
+
+	@BeforeEach
+	void setUp() {
+		outputStream = new ByteArrayOutputStream();
+		PrintStream out = new PrintStream(outputStream);
+		runner = new CalculateTargetAchievementRunner(out);
+	}
+
 	@Test
 	void created() {
-		PrintStream out = System.out;
-		InvestmentApplicationRunner runner = new CalculateTargetAchievementRunner(out);
-
 		Assertions.assertNotNull(runner);
 	}
 
 	@Test
 	void run_shouldPrintDate() {
-		PrintStream out = System.out;
-		InvestmentApplicationRunner runner = new CalculateTargetAchievementRunner(out);
-
 		runner.run();
+
+		String output = outputStream.toString();
+		Assertions.assertEquals("2025-10-01\n", output);
 	}
 }
