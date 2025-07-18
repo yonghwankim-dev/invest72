bfe8b4e8cef57003068cbfcf79677debf0619aeb
yonghwankim-dev <dragonbead95@naver.com>
2025-07-18 14:03:18 +0900

    update test - parameterizedTest

--- src/test/java/application/delegator/TargetAchievementReaderDelegatorTest.java
+++ src/test/java/application/delegator/TargetAchievementReaderDelegatorTest.java
@@ -2,15 +2,18 @@ package application.delegator;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import adapter.console.ui.BufferedWriterBasedGuidePrinter;
 import adapter.ui.GuidePrinter;
@@ -18,39 +21,62 @@ import application.builder.DefaultInvestmentRequestBuilder;
 import application.builder.InvestmentRequestBuilder;
 import application.reader.TargetAchievementRequestReader;
 import application.request.TargetAchievementRequest;
+import domain.type.TaxType;
 
 class TargetAchievementReaderDelegatorTest {
 
 	private InvestmentReaderDelegator<TargetAchievementRequest> delegator;
+	private TargetAchievementRequestReader reader;
+	private GuidePrinter guidePrinter;
+	private InvestmentRequestBuilder investmentRequestBuilder;
+
+	public static Stream<Arguments> targetAchievementInputFileSource() {
+		TargetAchievementRequest expected1 = TargetAchievementRequest.builder()
+			.targetAmount(10_000_000)
+			.monthlyInvestmentAmount(1_000_000)
+			.interestRate(0.05)
+			.taxType(TaxType.STANDARD.getDescription())
+			.taxRate(0.154)
+			.build();
+		TargetAchievementRequest expected2 = TargetAchievementRequest.builder()
+			.targetAmount(10_000_000)
+			.monthlyInvestmentAmount(1_000_000)
+			.interestRate(0.05)
+			.taxType(TaxType.NON_TAX.getDescription())
+			.taxRate(0.0)
+			.build();
+		return Stream.of(
+			Arguments.of("src/test/resources/target_achievement_case/case1/target_achievement_test_input1.txt",
+				expected1),
+			Arguments.of("src/test/resources/target_achievement_case/case2/target_achievement_test_input2.txt",
+				expected2)
+		);
+	}
 
 	@BeforeEach
 	void setUp() {
-		InvestmentRequestBuilder investmentRequestBuilder = new DefaultInvestmentRequestBuilder();
-		String input = String.join(System.lineSeparator(),
-			"10000000", // targetAmount
-			"1000000", // monthlyInvestmentAmount
-			"5", // interestRate
-			"비과세", // taxType
-			"0.0" // taxRate
-		);
-		InputStream in = new ByteArrayInputStream(input.getBytes());
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-		GuidePrinter guidePrinter = new BufferedWriterBasedGuidePrinter(
+		investmentRequestBuilder = new DefaultInvestmentRequestBuilder();
+		guidePrinter = new BufferedWriterBasedGuidePrinter(
 			new BufferedWriter(new OutputStreamWriter(System.out)));
-		TargetAchievementRequestReader reader = new TargetAchievementRequestReader(bufferedReader, guidePrinter);
 		delegator = new TargetAchievementReaderDelegator(investmentRequestBuilder, reader);
 	}
 
-	@Test
-	void readInvestmentRequest() throws IOException {
+	@ParameterizedTest
+	@MethodSource(value = "targetAchievementInputFileSource")
+	void readInvestmentRequest(String inputFilePath, TargetAchievementRequest expected) throws IOException {
+		InputStream in = new FileInputStream(inputFilePath);
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+		reader = new TargetAchievementRequestReader(bufferedReader, guidePrinter);
+		delegator = new TargetAchievementReaderDelegator(investmentRequestBuilder, reader);
+
 		TargetAchievementRequest request = delegator.readInvestmentRequest();
 
 		Assertions.assertNotNull(request);
-		Assertions.assertEquals(0, request.initialCapital());
-		Assertions.assertEquals(10_000_000, request.targetAmount());
-		Assertions.assertEquals(1_000_000, request.monthlyInvestmentAmount());
-		Assertions.assertEquals(0.05, request.interestRate());
-		Assertions.assertEquals("비과세", request.taxType());
-		Assertions.assertEquals(0.0, request.taxRate());
+		Assertions.assertEquals(expected.initialCapital(), request.initialCapital());
+		Assertions.assertEquals(expected.targetAmount(), request.targetAmount());
+		Assertions.assertEquals(expected.monthlyInvestmentAmount(), request.monthlyInvestmentAmount());
+		Assertions.assertEquals(expected.interestRate(), request.interestRate());
+		Assertions.assertEquals(expected.taxType(), request.taxType());
+		Assertions.assertEquals(expected.taxRate(), request.taxRate());
 	}
 }
