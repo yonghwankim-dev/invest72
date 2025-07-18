2c7ebc82e7a9b4ffd1f85a49332e2d95faf64a1b
yonghwankim-dev <dragonbead95@naver.com>
2025-07-18 12:07:14 +0900

    remove unused method - InvestReader

--- src/main/java/application/reader/InvestReader.java
+++ src/main/java/application/reader/InvestReader.java
@@ -8,16 +8,4 @@ public interface InvestReader {
 	String readFixedDepositAmount() throws IOException;
 
 	String readInstallmentSavingAmount() throws IOException;
-
-	String readPeriodType() throws IOException;
-
-	int readPeriodValue() throws IOException;
-
-	String readInterestType() throws IOException;
-
-	double readAnnualInterestRate() throws IOException;
-
-	String readTaxType() throws IOException;
-
-	double readTaxRate() throws IOException;
 }
