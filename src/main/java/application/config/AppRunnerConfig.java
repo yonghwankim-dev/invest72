ba9607887757c82ff1bbaa344cf3e970bfb67091
yonghwankim-dev <dragonbead95@naver.com>
2025-07-18 14:22:58 +0900

    imple createCalculateTargetAchievementRunner

--- src/main/java/application/config/AppRunnerConfig.java
+++ src/main/java/application/config/AppRunnerConfig.java
@@ -1,9 +1,12 @@
 package application.config;
 
 import adapter.InvestmentApplicationRunner;
+import adapter.console.CalculateTargetAchievementRunner;
 
 public interface AppRunnerConfig {
 	InvestmentApplicationRunner createCalculateInvestmentRunner();
 
 	InvestmentApplicationRunner createCalculateMonthlyInvestmentRunner();
+
+	CalculateTargetAchievementRunner createCalculateTargetAchievementRunner();
 }
