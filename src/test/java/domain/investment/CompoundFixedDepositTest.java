5b97812a371dba2802c9938a8d9937388c0cd4de
yonghwankim-dev <dragonbead95@naver.com>
2025-07-23 16:31:56 +0900

    delete test
    csv file을 이용한 테스트를 추가하기 위해서 기존 불필요한 테스트 제거

--- src/test/java/domain/investment/CompoundFixedDepositTest.java
+++ src/test/java/domain/investment/CompoundFixedDepositTest.java
@@ -1,15 +1,6 @@
 package domain.investment;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import domain.amount.FixedDepositAmount;
 import domain.amount.LumpSumInvestmentAmount;
@@ -31,125 +22,17 @@ class CompoundFixedDepositTest {
 	private Taxable taxable;
 	private Investment investment;
 
-	public static Stream<Arguments> interestSource() {
-		return Stream.of(
-			Arguments.of(1, 4_167),
-			Arguments.of(2, 8351)
-		);
-	}
-
 	@BeforeEach
 	void setUp() {
 		depositAmount = new FixedDepositAmount(1_000_000);
 		interestRate = new AnnualInterestRate(0.05);
 		investPeriod = new YearlyInvestPeriod(1);
 		taxableFactory = new KoreanTaxableFactory();
-		taxable = taxableFactory.createNonTax();
-		investment = new CompoundFixedDeposit(
-			depositAmount,
-			investPeriod, interestRate,
-			taxable
-		);
-	}
-
-	@Test
-	void shouldReturnPrincipal() {
-		int principal = investment.getPrincipal();
-
-		int expectedPrincipal = 1_000_000;
-		assertEquals(expectedPrincipal, principal);
-	}
-
-	@ParameterizedTest
-	@ValueSource(ints = {1, 12})
-	void shouldReturnPrincipal_givenMonth(int month) {
-		int principal = investment.getPrincipal(month);
-
-		int expectedPrincipal = 1_000_000;
-		assertEquals(expectedPrincipal, principal);
-	}
-
-	@Test
-	void shouldReturnInterest_whenExpiration() {
-		int interest = investment.getInterest();
-
-		int expectedInterest = 51_162;
-		assertEquals(expectedInterest, interest);
-	}
-
-	@ParameterizedTest
-	@MethodSource(value = "interestSource")
-	void shouldReturnInterest_whenMonth(int month, int expectedInterest) {
-		int interest = investment.getInterest(month);
-
-		assertEquals(expectedInterest, interest);
-	}
-
-	@Test
-	void shouldReturnAmount_whenInterestRateIsCompound() {
-		int amount = investment.getTotalProfit();
-
-		int expectedAmount = 1_051_162;
-		assertEquals(expectedAmount, amount);
-	}
-
-	@Test
-	void shouldReturnAmount_whenInterestRateIsZero() {
-		interestRate = new AnnualInterestRate(0);
-		investment = new CompoundFixedDeposit(
-			depositAmount,
-			investPeriod, interestRate,
-			taxable
-		);
-
-		int amount = investment.getTotalProfit();
-
-		int expectedAmount = 1_000_000;
-		assertEquals(expectedAmount, amount);
-	}
-
-	@Test
-	void shouldReturnAmount_whenYearsIsZero() {
-		investPeriod = new YearlyInvestPeriod(0);
-		investment = new CompoundFixedDeposit(
-			depositAmount,
-			investPeriod, interestRate,
-			taxable
-		);
-
-		int amount = investment.getTotalProfit();
-
-		int expectedAmount = 1_000_000;
-		assertEquals(expectedAmount, amount);
-	}
-
-	@Test
-	void shouldReturnZeroAmount_whenAmountIsZero() {
-		depositAmount = new FixedDepositAmount(0);
-		investment = new CompoundFixedDeposit(
-			depositAmount,
-			investPeriod, interestRate,
-			taxable
-		);
-
-		int amount = investment.getTotalProfit();
-
-		int expectedAmount = 0;
-		assertEquals(expectedAmount, amount);
-	}
-
-	@Test
-	void shouldReturnAmount_whenInterestRateIsCompoundAndStandardTax() {
 		taxable = taxableFactory.createStandardTax(new FixedTaxRate(0.154));
 		investment = new CompoundFixedDeposit(
 			depositAmount,
 			investPeriod, interestRate,
 			taxable
 		);
-
-		int amount = investment.getTotalProfit();
-
-		int expectedAmount = 1_043_283;
-		assertEquals(expectedAmount, amount);
 	}
 }
