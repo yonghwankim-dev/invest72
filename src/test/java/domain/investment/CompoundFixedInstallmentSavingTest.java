53a948bf65a4c3d46df15b6aac57160db3a5db20
yonghwankim-dev <dragonbead95@naver.com>
2025-07-24 12:20:28 +0900

    introduce varaible

--- src/test/java/domain/investment/CompoundFixedInstallmentSavingTest.java
+++ src/test/java/domain/investment/CompoundFixedInstallmentSavingTest.java
@@ -22,20 +22,15 @@ import domain.tax.factory.TaxableFactory;
 
 class CompoundFixedInstallmentSavingTest {
 
-	private InstallmentInvestmentAmount investmentAmount; // 월 투자 금액(원)
-	private InvestPeriod investPeriod; // 투자 기간
-	private InterestRate annualInterestRateRate; // 연 수익율
-	private TaxableFactory taxableFactory;
-	private Taxable taxable; // 세금 적용 방식
-	private CompoundFixedInstallmentSaving investment;
+	private Investment investment;
 
 	@BeforeEach
 	void setUp() {
-		investmentAmount = new MonthlyInstallmentInvestmentAmount(1_000_000);
-		investPeriod = new MonthlyInvestPeriod(12);
-		annualInterestRateRate = new AnnualInterestRate(0.05);
-		taxableFactory = new KoreanTaxableFactory();
-		taxable = taxableFactory.createStandardTax(new FixedTaxRate(0.154));
+		InstallmentInvestmentAmount investmentAmount = new MonthlyInstallmentInvestmentAmount(1_000_000);
+		InvestPeriod investPeriod = new MonthlyInvestPeriod(12);
+		InterestRate annualInterestRateRate = new AnnualInterestRate(0.05);
+		TaxableFactory taxableFactory = new KoreanTaxableFactory();
+		Taxable taxable = taxableFactory.createStandardTax(new FixedTaxRate(0.154));
 		investment = new CompoundFixedInstallmentSaving(investmentAmount, investPeriod, annualInterestRateRate,
 			taxable);
 
@@ -55,7 +50,7 @@ class CompoundFixedInstallmentSavingTest {
 		assertEquals(expectedTax, tax);
 		assertEquals(expectedTotalProfit, totalProfit);
 	}
-	
+
 	@Test
 	void shouldReturnTotalProfit() {
 		assertEquals(12_279_194, investment.getTotalProfit());
