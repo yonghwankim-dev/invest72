bc48ac4b21a2a04eb39dfe8e782364f5f7c71af6
yonghwankim-dev <dragonbead95@naver.com>
2025-07-31 13:23:35 +0900

    delete InvestmentFactory<R>

--- src/main/java/co/invest72/investment/domain/investment/InvestmentFactory.java
+++
@@ -1,7 +0,0 @@
-package co.invest72.investment.domain.investment;
-
-import application.request.CalculateInvestmentRequest;
-
-public interface InvestmentFactory<R> {
-	R createBy(CalculateInvestmentRequest request);
-}
