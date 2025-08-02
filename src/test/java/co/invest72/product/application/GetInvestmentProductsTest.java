148453cbbd8af77e57112d0bdbc7f0a44dc90900
yonghwankim-dev <dragonbead95@naver.com>
2025-08-02 13:42:09 +0900

    create entity sampple

--- src/test/java/co/invest72/product/application/GetInvestmentProductsTest.java
+++ src/test/java/co/invest72/product/application/GetInvestmentProductsTest.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import co.invest72.investment.domain.interest.InterestType;
+import co.invest72.investment.domain.investment.InvestmentType;
+import co.invest72.investment.domain.tax.TaxType;
+import co.invest72.product.domain.InvestmentProductEntity;
 import co.invest72.product.domain.InvestmentProductRepository;
 import co.invest72.product.persistence.InvestmentProductInMemoryRepository;
 
@@ -13,11 +17,28 @@ class GetInvestmentProductsTest {
 	@Test
 	void listInvestment() {
 		InvestmentProductRepository repository = new InvestmentProductInMemoryRepository();
-		GetInvestmentProducts getInvestmentProducts = new GetInvestmentProducts(repository);
 		String uid = "test-uid";
+		InvestmentProductEntity investmentProductEntity = createSimpleFixedDepositInvestmentProductEntity(uid);
+		repository.save(investmentProductEntity);
+
+		GetInvestmentProducts getInvestmentProducts = new GetInvestmentProducts(repository);
 
 		List<InvestmentProductDto> result = getInvestmentProducts.listInvestment(uid);
 
 		Assertions.assertNotNull(result);
+		Assertions.assertEquals(1, result.size());
+	}
+
+	private InvestmentProductEntity createSimpleFixedDepositInvestmentProductEntity(String uid) {
+		return InvestmentProductEntity.builder()
+			.uid(uid)
+			.investmentType(InvestmentType.FIXED_DEPOSIT)
+			.investmentAmount(1_000_000)
+			.interestType(InterestType.SIMPLE)
+			.annualRate(0.05)
+			.investmentPeriodMonth(12)
+			.taxType(TaxType.STANDARD)
+			.taxRate(0.154)
+			.build();
 	}
 }
