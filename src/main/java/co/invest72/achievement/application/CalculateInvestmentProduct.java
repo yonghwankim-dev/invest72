1ac54b52894bdb5fb5c007b2e1834cf6aa80413d
yonghwankim-dev <dragonbead95@naver.com>
2025-08-10 15:31:12 +0900

    add test - CalculateInvestmentProduct.calculate()

--- src/main/java/co/invest72/achievement/application/CalculateInvestmentProduct.java
+++ src/main/java/co/invest72/achievement/application/CalculateInvestmentProduct.java
@@ -1,2 +1,23 @@
-package co.invest72.achievement.application;public class CalculateInvestmentProduct {
+package co.invest72.achievement.application;
+
+import java.time.LocalDate;
+
+import co.invest72.product.domain.InvestmentProductEntity;
+
+public class CalculateInvestmentProduct {
+
+	public CalculateInvestmentProductResponse calculate(
+		InvestmentProductEntity product) {
+		LocalDate achieveDate = LocalDate.of(2025, 1, 1);
+		long totalAccumulatedAmount = 1_050_000L;
+		CalculateInvestmentProductResponse response = new CalculateInvestmentProductResponse(achieveDate,
+			totalAccumulatedAmount);
+		return response;
+	}
+
+	public record CalculateInvestmentProductResponse(
+		LocalDate achieveDate,
+		long totalAccumulatedAmount
+	) {
+	}
 }
--- src/test/java/co/invest72/achievement/application/CalculateInvestmentProductTest.java
+++ src/test/java/co/invest72/achievement/application/CalculateInvestmentProductTest.java
@@ -1,4 +1,47 @@
+package co.invest72.achievement.application;
+
+import static co.invest72.achievement.application.CalculateInvestmentProduct.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import co.invest72.investment.domain.interest.InterestType;
+import co.invest72.investment.domain.investment.InvestmentType;
+import co.invest72.investment.domain.tax.TaxType;
+import co.invest72.product.domain.AmountType;
+import co.invest72.product.domain.InvestmentProductEntity;
+
 class CalculateInvestmentProductTest {
-  
+
+	@Test
+	void canCreated() {
+		CalculateInvestmentProduct useCase = new CalculateInvestmentProduct();
+		assertNotNull(useCase);
+	}
+
+	@Test
+	void calculate() {
+		CalculateInvestmentProduct useCase = new CalculateInvestmentProduct();
+		InvestmentProductEntity product = InvestmentProductEntity.builder()
+			.id(1L)
+			.uid("test-uid")
+			.investmentType(InvestmentType.FIXED_DEPOSIT)
+			.amountType(AmountType.ONE_TIME)
+			.investmentAmount(1_000_000)
+			.interestType(InterestType.COMPOUND)
+			.annualRate(0.05)
+			.investmentPeriodMonth(12)
+			.taxType(TaxType.NON_TAX)
+			.taxRate(0.0)
+			.build();
+
+		CalculateInvestmentProductResponse response = useCase.calculate(product);
+
+		CalculateInvestmentProductResponse expected = new CalculateInvestmentProductResponse(LocalDate.of(2025, 1, 1),
+			1_050_000L);
+		Assertions.assertEquals(expected, response);
+	}
 }
