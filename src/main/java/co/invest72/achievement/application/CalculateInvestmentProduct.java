9829e4520c2803bd751f0a2ea16e9623c6a8e620
yonghwankim-dev <dragonbead95@naver.com>
2025-08-12 15:52:20 +0900

    update code - calculate
    하나의 투자 상품에 대하여 목표 금액에 도달하기 위한 개월수를 계산함
    
    추후 여러개의 투자 상품으로 변경 예정

--- src/main/java/co/invest72/achievement/application/CalculateInvestmentProduct.java
+++ src/main/java/co/invest72/achievement/application/CalculateInvestmentProduct.java
@@ -2,22 +2,37 @@ package co.invest72.achievement.application;
 
 import java.time.LocalDate;
 
-import co.invest72.achievement.domain.time.AchievementInvestmentCalculator;
+import co.invest72.investment.application.InvestmentFactory;
+import co.invest72.investment.domain.Investment;
 import co.invest72.product.domain.InvestmentProductEntity;
 
 public class CalculateInvestmentProduct {
-	private final AchievementInvestmentCalculator calculator;
+	private final InvestmentFactory investmentFactory;
 
-	public CalculateInvestmentProduct(AchievementInvestmentCalculator calculator) {
-		this.calculator = calculator;
+	public CalculateInvestmentProduct(InvestmentFactory investmentFactory) {
+		this.investmentFactory = investmentFactory;
 	}
 
 	// todo: implement
 	public CalculateInvestmentProductResponse calculate(
-		InvestmentProductEntity product) {
-		
-		LocalDate achieveDate = LocalDate.of(2025, 1, 1);
-		long totalAccumulatedAmount = 1_050_000L;
+		InvestmentProductEntity product, int targetAmount) {
+
+		// 투자 상품이 존재하고 투자 상품이 목표 금액에 달성하기 위해서 필요한 개월수 및 누적금액을 계산
+		Investment investment = investmentFactory.createBy(product);
+
+		// targetAmount에 도달하기 위한 개월 수를 계산
+		int month = 1;
+		try {
+			while (investment.getTotalProfit(month) < targetAmount) {
+				month++;
+			}
+		} catch (IllegalArgumentException e) {
+			// 투자 상품이 목표 금액에 도달할 수 없는 경우
+			throw new IllegalArgumentException("투자 상품이 목표 금액에 도달할 수 없습니다.");
+		}
+
+		LocalDate achieveDate = product.getStartDate().plusMonths(month);
+		long totalAccumulatedAmount = investment.getTotalProfit(month);
 		return new CalculateInvestmentProductResponse(achieveDate,
 			totalAccumulatedAmount);
 	}
--- src/test/java/co/invest72/achievement/application/CalculateInvestmentProductTest.java
+++ src/test/java/co/invest72/achievement/application/CalculateInvestmentProductTest.java
@@ -8,14 +8,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import co.invest72.achievement.domain.time.AchievementInvestmentCalculator;
-import co.invest72.investment.domain.TaxableFactory;
-import co.invest72.investment.domain.TaxableResolver;
+import co.invest72.investment.application.InvestmentFactory;
 import co.invest72.investment.domain.interest.InterestType;
 import co.invest72.investment.domain.investment.InvestmentType;
-import co.invest72.investment.domain.tax.KoreanTaxableFactory;
 import co.invest72.investment.domain.tax.TaxType;
-import co.invest72.investment.domain.tax.resolver.KoreanStringBasedTaxableResolver;
 import co.invest72.product.domain.AmountType;
 import co.invest72.product.domain.InvestmentProductEntity;
 
@@ -25,10 +21,8 @@ class CalculateInvestmentProductTest {
 
 	@BeforeEach
 	void setUp() {
-		TaxableFactory taxableFactory = new KoreanTaxableFactory();
-		TaxableResolver taxableResolver = new KoreanStringBasedTaxableResolver(taxableFactory);
-		AchievementInvestmentCalculator calculator = new AchievementInvestmentCalculator(taxableResolver);
-		useCase = new CalculateInvestmentProduct(calculator);
+		InvestmentFactory investmentFactory = new InvestmentFactory();
+		useCase = new CalculateInvestmentProduct(investmentFactory);
 	}
 
 	@Test
@@ -39,15 +33,16 @@ class CalculateInvestmentProductTest {
 			.investmentType(InvestmentType.FIXED_DEPOSIT)
 			.amountType(AmountType.ONE_TIME)
 			.investmentAmount(1_000_000)
-			.interestType(InterestType.COMPOUND)
+			.interestType(InterestType.SIMPLE)
 			.annualRate(0.05)
 			.investmentPeriodMonth(12)
 			.taxType(TaxType.NON_TAX)
 			.taxRate(0.0)
 			.startDate(LocalDate.of(2024, 1, 1))
 			.build();
+		int targetAmount = 1_050_000;
 
-		CalculateInvestmentProductResponse response = useCase.calculate(product);
+		CalculateInvestmentProductResponse response = useCase.calculate(product, targetAmount);
 
 		CalculateInvestmentProductResponse expected = new CalculateInvestmentProductResponse(LocalDate.of(2025, 1, 1),
 			1_050_000L);
