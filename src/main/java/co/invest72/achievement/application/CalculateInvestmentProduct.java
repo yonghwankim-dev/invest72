b8ce3c6490b95b8c81dc5db280044ae858db48b6
yonghwankim-dev <dragonbead95@naver.com>
2025-09-27 15:29:41 +0900

    extract method

--- src/main/java/co/invest72/achievement/application/CalculateInvestmentProduct.java
+++ src/main/java/co/invest72/achievement/application/CalculateInvestmentProduct.java
@@ -19,31 +19,36 @@ public class CalculateInvestmentProduct {
 
 	public CalculateInvestmentProductResponse calculate(
 		int targetAmount, InvestmentProductEntity... products) {
-		List<Investment> investments = Arrays.stream(products)
-			.map(investmentFactory::createBy)
-			.toList();
+		List<Investment> investments = getInvestments(products);
 
 		// 목표금액(targetAmount)에 도달하기 위한 개월(month)수 계산
 		int month = calMonth(targetAmount, investments);
-		logger.info("목표금액 " + targetAmount + "원에 도달하기 위한 개월 수: " + month + "개월");
 
 		// 모든 상품 중 가장 빠른 시작일을 구함
-		LocalDate earliestStartDate = Arrays.stream(products)
+		// 목표금액에 도달하는 날짜 계산
+		LocalDate achieveDate = getEarliestStartDate(products).plusMonths(month);
+
+		long totalAccumulatedAmount = calTotalProfit(investments, month);
+		return new CalculateInvestmentProductResponse(achieveDate, totalAccumulatedAmount);
+	}
+
+	private List<Investment> getInvestments(InvestmentProductEntity[] products) {
+		return Arrays.stream(products)
+			.map(investmentFactory::createBy)
+			.toList();
+	}
+
+	private static LocalDate getEarliestStartDate(InvestmentProductEntity[] products) {
+		return Arrays.stream(products)
 			.map(InvestmentProductEntity::getStartDate)
 			.min(LocalDate::compareTo)
 			.orElseThrow(() -> new IllegalArgumentException("상품의 시작일을 찾을 수 없습니다."));
-		// 목표금액에 도달하는 날짜 계산
-		LocalDate achieveDate = earliestStartDate.plusMonths(month);
-
-		long totalAccumulatedAmount = sumTotalProfit(investments, month);
-		return new CalculateInvestmentProductResponse(achieveDate,
-			totalAccumulatedAmount);
 	}
 
 	private int calMonth(int targetAmount, List<Investment> investments) {
 		int month = 1;
 		for (; month <= 999; month++) {
-			int sum = sumTotalProfit(investments, month);
+			int sum = calTotalProfit(investments, month);
 			if (sum >= targetAmount) {
 				return month;
 			}
@@ -51,7 +56,7 @@ public class CalculateInvestmentProduct {
 		throw new IllegalArgumentException("목표금액에 도달할 수 없습니다.");
 	}
 
-	private int sumTotalProfit(List<Investment> investments, int month) {
+	private int calTotalProfit(List<Investment> investments, int month) {
 		return investments.stream()
 			.mapToInt(investment -> {
 				int effectiveMonth = Math.min(month, investment.getFinalMonth());
