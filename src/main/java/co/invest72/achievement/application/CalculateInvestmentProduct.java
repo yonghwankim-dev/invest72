3c5c28255465a9c65572fe55e16a467e4b5859c7
yonghwankim-dev <dragonbead95@naver.com>
2025-09-27 12:37:47 +0900

    update code - achieveDate code

--- src/main/java/co/invest72/achievement/application/CalculateInvestmentProduct.java
+++ src/main/java/co/invest72/achievement/application/CalculateInvestmentProduct.java
@@ -3,12 +3,14 @@ package co.invest72.achievement.application;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 import co.invest72.investment.application.InvestmentFactory;
 import co.invest72.investment.domain.Investment;
 import co.invest72.product.domain.InvestmentProductEntity;
 
 public class CalculateInvestmentProduct {
+	private static final Logger logger = Logger.getLogger(CalculateInvestmentProduct.class.getName());
 	private final InvestmentFactory investmentFactory;
 
 	public CalculateInvestmentProduct(InvestmentFactory investmentFactory) {
@@ -21,8 +23,11 @@ public class CalculateInvestmentProduct {
 			.map(investmentFactory::createBy)
 			.toList();
 
+		// 목표금액(targetAmount)에 도달하기 위한 개월(month)수 계산
 		int month = calMonth(targetAmount, investments);
-		LocalDate achieveDate = calAchieveDate(product, month);
+		logger.info("목표금액 " + targetAmount + "원에 도달하기 위한 개월 수: " + month + "개월");
+		// 목표금액에 도달하는 날짜 계산
+		LocalDate achieveDate = products[0].getStartDate().plusMonths(month);
 
 		long totalAccumulatedAmount = sumTotalProfit(investments, month);
 		return new CalculateInvestmentProductResponse(achieveDate,
@@ -41,10 +46,6 @@ public class CalculateInvestmentProduct {
 		return investments.stream()
 			.mapToInt(investment -> investment.getTotalProfit(month))
 			.sum();
-	}
-
-	private static LocalDate calAchieveDate(InvestmentProductEntity product, int month) {
-		return product.getStartDate().plusMonths(month);
 	}
 
 	public record CalculateInvestmentProductResponse(
