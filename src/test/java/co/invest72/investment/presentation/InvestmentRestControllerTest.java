2b42e3b45b4e3e506a729a508bfd82c3d03f1a32
yonghwankim-dev <dragonbead95@naver.com>
2025-11-10 17:14:18 +0900

    delete tests

--- src/test/java/co/invest72/investment/presentation/InvestmentRestControllerTest.java
+++ src/test/java/co/invest72/investment/presentation/InvestmentRestControllerTest.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -24,8 +23,6 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import co.invest72.investment.domain.amount.AmountType;
-import co.invest72.investment.presentation.request.CalculateInvestmentRequest;
 import util.TestFileUtils;
 
 @SpringBootTest
@@ -74,52 +71,6 @@ class InvestmentRestControllerTest {
 	@BeforeEach
 	void setUp() {
 		this.mockMvc = createMockMvc();
-	}
-
-	@Test
-	void calculateExpiration_whenTypeIsFixedDeposit() throws Exception {
-		CalculateInvestmentRequest request = CalculateInvestmentRequest.builder()
-			.type("예금")
-			.amountType(AmountType.ONE_TIME.getDescription())
-			.amount(1_000_000)
-			.periodType("년")
-			.periodValue(1)
-			.interestType("단리")
-			.annualInterestRate(0.05)
-			.taxType("일반과세")
-			.taxRate(0.154)
-			.build();
-		mockMvc.perform(post("/investments/calculate/expiration")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(request)))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.totalProfitAmount").value(1042300))
-			.andExpect(jsonPath("$.totalPrincipalAmount").value(1000000))
-			.andExpect(jsonPath("$.interest").value(50000))
-			.andExpect(jsonPath("$.tax").value(7700));
-	}
-
-	@Test
-	void calculateExpiration_whenTypeIsInstallment() throws Exception {
-		CalculateInvestmentRequest request = CalculateInvestmentRequest.builder()
-			.type("적금")
-			.amountType(AmountType.MONTHLY.getDescription())
-			.amount(1_000_000)
-			.periodType("년")
-			.periodValue(1)
-			.interestType("단리")
-			.annualInterestRate(0.05)
-			.taxType("일반과세")
-			.taxRate(0.154)
-			.build();
-		mockMvc.perform(post("/investments/calculate/expiration")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(request)))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.totalProfitAmount").value(12274950))
-			.andExpect(jsonPath("$.totalPrincipalAmount").value(12000000))
-			.andExpect(jsonPath("$.interest").value(325000))
-			.andExpect(jsonPath("$.tax").value(50050));
 	}
 
 	@ParameterizedTest
