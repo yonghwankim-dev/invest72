069293d9035a0d3ffada576da0bbe2bb7f743902
yonghwankim-dev <dragonbead95@naver.com>
2025-11-10 12:08:08 +0900

    change type - primitive type to wrapper type

--- src/main/java/co/invest72/investment/presentation/request/CalculateInvestmentRequest.java
+++ src/main/java/co/invest72/investment/presentation/request/CalculateInvestmentRequest.java
@@ -16,25 +16,24 @@ public class CalculateInvestmentRequest {
 	private String amountType;
 	private Integer amount;
 	private String periodType;
-	private int periodValue;
+	private Integer periodValue;
 	private String interestType;
-	private double annualInterestRate;
+	private Double annualInterestRate;
 	private String taxType;
-	private double taxRate;
+	private Double taxRate;
 
 	@Builder
 	private CalculateInvestmentRequest(String type, String amountType, Integer amount, String periodType,
-		int periodValue,
-		String interestType, double annualInterestRate, String taxType, double taxRate) {
+		Integer periodValue, String interestType, Double annualInterestRate, String taxType, Double taxRate) {
 		this.type = Objects.requireNonNull(type, "type must not be null");
 		this.amountType = Objects.requireNonNull(amountType, "amountType must not be null");
-		this.amount = amount;
+		this.amount = Objects.requireNonNull(amount, "amount must not be null");
 		this.periodType = Objects.requireNonNull(periodType, "periodType must not be null");
-		this.periodValue = periodValue;
+		this.periodValue = Objects.requireNonNull(periodValue, "periodValue must not be null");
 		this.interestType = Objects.requireNonNull(interestType, "interestType must not be null");
-		this.annualInterestRate = annualInterestRate;
+		this.annualInterestRate = Objects.requireNonNull(annualInterestRate, "annualInterestRate must not be null");
 		this.taxType = Objects.requireNonNull(taxType, "taxType must not be null");
-		this.taxRate = taxRate;
+		this.taxRate = Objects.requireNonNull(taxRate, "taxRate must not be null");
 	}
 
 	@Override
