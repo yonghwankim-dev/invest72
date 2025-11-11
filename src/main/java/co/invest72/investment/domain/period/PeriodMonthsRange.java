d48491a5d32c1939dba113f9dba1d42d20f5061c
yonghwankim-dev <dragonbead95@naver.com>
2025-11-11 11:38:47 +0900

    add test - PeriodMonthsRange newInstance

--- src/main/java/co/invest72/investment/domain/period/PeriodMonthsRange.java
+++ src/main/java/co/invest72/investment/domain/period/PeriodMonthsRange.java
@@ -11,7 +11,7 @@ public class PeriodMonthsRange implements PeriodRange {
 		if (this.months < 0) {
 			throw new IllegalArgumentException("Investment period must be greater than or equal to zero.");
 		}
-		if (this.months > 23976) { // 999 years
+		if (this.months > 11988) { // 999 years
 			throw new IllegalArgumentException("Investment period must be less than or equal to 23976 months.");
 		}
 	}
