d2dc8188f22ce1d551d1ec06c8861cedef10bd68
yonghwankim-dev <dragonbead95@naver.com>
2025-11-14 13:21:35 +0900

    imple calculate

--- src/main/java/co/invest72/investment/application/CalculateGoalInvestment.java
+++ src/main/java/co/invest72/investment/application/CalculateGoalInvestment.java
@@ -1,10 +1,32 @@
 package co.invest72.investment.application;
 
-import co.invest72.investment.application.dto.CalculateGoalDto;
+import java.time.LocalDate;
 
+import co.invest72.investment.application.dto.CalculateGoalDto;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class CalculateGoalInvestment {
 
 	public int calculate(CalculateGoalDto dto) {
-		return 9;
+		Integer monthlyInvestmentAmount = dto.getMonthlyInvestmentAmount();
+		Double annualInterestRate = dto.getAnnualInterestRate();
+		Integer goalAmount = dto.getGoalAmount();
+		LocalDate startDate = dto.getStartDate();
+
+		int months = 0;
+		int totalAmount = 0;
+		while (totalAmount < goalAmount) {
+			totalAmount += monthlyInvestmentAmount;
+			double monthlyInterest = totalAmount * (annualInterestRate / 12);
+			totalAmount += (int)monthlyInterest;
+			log.info("After month {}: totalAmount = {}", months, totalAmount);
+			if (totalAmount >= goalAmount) {
+				break;
+			}
+			months++;
+		}
+
+		return months;
 	}
 }
