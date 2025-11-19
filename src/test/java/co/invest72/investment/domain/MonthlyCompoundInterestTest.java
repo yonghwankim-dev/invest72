a6ca5033c858edaa6086d52bafa7e718c4703316
yonghwankim-dev <dragonbead95@naver.com>
2025-11-19 12:40:56 +0900

    introduce field - Investment

--- src/test/java/co/invest72/investment/domain/MonthlyCompoundInterestTest.java
+++ src/test/java/co/invest72/investment/domain/MonthlyCompoundInterestTest.java
@@ -1,6 +1,7 @@
 package co.invest72.investment.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import co.invest72.investment.domain.amount.FixedDepositAmount;
@@ -10,18 +11,23 @@ import co.invest72.investment.domain.period.YearlyInvestPeriod;
 import co.invest72.investment.domain.tax.KoreanTaxableFactory;
 
 class MonthlyCompoundInterestTest {
-	
-	@Test
-	void canCreated() {
+
+	private Investment investment;
+
+	@BeforeEach
+	void setUp() {
 		TaxableFactory taxableFactory = new KoreanTaxableFactory();
-		Investment investment = MonthlyCompoundInterest.builder()
+		investment = MonthlyCompoundInterest.builder()
 			.initialAmount(new FixedDepositAmount(0))
 			.monthlyAmount(new MonthlyAmount(1_000_000))
 			.investPeriod(new YearlyInvestPeriod(1))
 			.interestRate(new AnnualInterestRate(0.05))
 			.taxable(taxableFactory.createNonTax())
 			.build();
+	}
 
+	@Test
+	void canCreated() {
 		Assertions.assertThat(investment).isNotNull();
 	}
 
