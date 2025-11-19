3394d0f5f062d9f776083e990fb66c793e2bd46f
yonghwankim-dev <dragonbead95@naver.com>
2025-11-19 13:50:00 +0900

    refactor : use MonthlyCompoundInterest instance

--- src/main/java/co/invest72/investment/application/CalculateMonthlyCompoundInterest.java
+++ src/main/java/co/invest72/investment/application/CalculateMonthlyCompoundInterest.java
@@ -4,46 +4,33 @@ import java.math.BigDecimal;
 
 import co.invest72.investment.application.dto.CalculateMonthlyCompoundInterestDto;
 import co.invest72.investment.application.dto.CalculateMonthlyCompoundInterestResultDto;
-import co.invest72.investment.domain.InterestRate;
-import co.invest72.investment.domain.InvestPeriod;
-import co.invest72.investment.domain.InvestmentAmount;
+import co.invest72.investment.domain.Investment;
+import co.invest72.investment.domain.MonthlyCompoundInterest;
 import co.invest72.investment.domain.amount.FixedDepositAmount;
 import co.invest72.investment.domain.amount.MonthlyAmount;
 import co.invest72.investment.domain.interest.AnnualInterestRate;
 import co.invest72.investment.domain.period.YearlyInvestPeriod;
+import co.invest72.investment.domain.tax.NonTax;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CalculateMonthlyCompoundInterest {
 
 	public CalculateMonthlyCompoundInterestResultDto calculate(CalculateMonthlyCompoundInterestDto dto) {
-		InvestmentAmount initialAmount = new FixedDepositAmount(dto.getInitialAmount());
-		InvestmentAmount investmentAmount = new MonthlyAmount(dto.getMonthlyDeposit());
-		InvestPeriod investPeriod = new YearlyInvestPeriod(dto.getInvestmentYears());
-		InterestRate interestRate = new AnnualInterestRate(dto.getAnnualInterestRate());
-
-		BigDecimal principal = initialAmount.getAmount();
-		BigDecimal interest = interestRate.calMonthlyInterest(principal.intValue());
-		BigDecimal totalProfit = principal.add(interest);
-
-		BigDecimal totalInterest = BigDecimal.ZERO;
-		for (int months = 2; months <= investPeriod.getMonths(); months++) {
-			principal = totalProfit.add(investmentAmount.getAmount());
-			interest = interestRate.calMonthlyInterest(principal.intValue());
-			totalProfit = principal.add(interest);
-
-			totalInterest = totalInterest.add(interest);
-			log.info("Month: {}, Principal: {}, Interest: {}, MonthTotalProfit: {}", months, principal, interest,
-				totalProfit);
-		}
-		BigDecimal months = BigDecimal.valueOf(investPeriod.getMonths() - 1);
-		BigDecimal totalPrincipal = calTotalPrincipal(initialAmount.getAmount(),
-			investmentAmount.getAmount(), months);
-
+		Investment investment = new MonthlyCompoundInterest(
+			new FixedDepositAmount(dto.getInitialAmount()),
+			new MonthlyAmount(dto.getMonthlyDeposit()),
+			new YearlyInvestPeriod(dto.getInvestmentYears()),
+			new AnnualInterestRate(dto.getAnnualInterestRate()),
+			new NonTax()
+		);
+		Integer totalPrincipal = investment.getPrincipal();
+		Integer totalInterest = investment.getInterest();
+		Integer totalProfit = investment.getTotalProfit();
 		return CalculateMonthlyCompoundInterestResultDto.builder()
-			.totalPrincipal(totalPrincipal.intValue())
-			.totalInterest(totalInterest.intValue())
-			.totalProfit(totalProfit.intValue())
+			.totalPrincipal(totalPrincipal)
+			.totalInterest(totalInterest)
+			.totalProfit(totalProfit)
 			.build();
 	}
 
